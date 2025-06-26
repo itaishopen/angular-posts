a2916b5bf71fd4af33237422b5888deb84a5d832
itai <itaishopen@gmail.com>
2025-06-26 22:58:27 +0300

    adding darkmode

--- src/app/component/post-grid/post-grid.component.ts
+++ src/app/component/post-grid/post-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
 import { Store } from '@ngrx/store';
 import {PostSquareComponent} from '../post-square/post-square.component';
 import {selectAllPosts, selectCurrentPostId, selectLoading} from '../../state/posts.selectors';
@@ -23,11 +23,21 @@ export class PostGridComponent{
   posts$;
   selectedId$;
   loading$
+  readonly isDark = signal<boolean>(JSON.parse(localStorage.getItem('darkMode') ?? 'false'));
   constructor(private store: Store) {
     this.store.dispatch(loadPosts());
     this.posts$ = this.store.select(selectAllPosts);
     this.selectedId$ = this.store.select(selectCurrentPostId);
     this.loading$ = this.store.select(selectLoading);
+    effect(() => {
+      document.body.classList.toggle('dark-theme', this.isDark());
+      localStorage.setItem('darkMode', JSON.stringify(this.isDark()));
+    });
+  }
+
+  toggle() {
+    console.log(this.isDark());
+    this.isDark.update(d => !d);
   }
 
   trackById(_: number, post: any) { return post.id; }
